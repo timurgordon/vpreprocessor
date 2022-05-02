429d8cff4554718d362f22125bbf4cb18d2d56ff
timurgordon <timurgordon@gmail.com>
2022-05-02 09:31:12 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,157 +1,31 @@
-# MDBook Presentation Preprocessor
-A preprocessor for utilizing an MDBook as slides
-for a presentation.
+# MDBook Preprocessor for Vlang
 
-The goal of this project is to allow the writing of one
-MDBook that can be used as both presentation and mdbook
-style website.
+A preprocessor parsing MDBook compatible with Vlang to enable preproccessing in Vlang
 
-See it in action [here](https://freemasen.github.io/mdbook-presentation-preprocessor/),
-the source for this is in the example directory.
+The goal of this project is to allow preprocessing with Vlang by formatting deserialized mdbook, and sending encoded object via rmb. The processed book is then received via rmb and formatted back to MDBook conventions ready for compilation.
 
-## Usage
-First you need to install the pre-processor by running the following
-command.
+## Work in progress
 
-```sh
-cargo install mdbook-presentation-preprocessor
+Currently one can format MDBooks into Vlang compatible structs.
+
+The purpose of this project is to overcome the incompatibilities between rust and vlang so that once can process MDBook objects in v.
+
+Right now, formatted book objects can be accessed via v over redis for processing. However, Reliable Message Bus will be used for messaging between rust and v which is currently under development.
+
+In future implementations this project will be shipped as a cargo package to enable easy mdbook preprocessing.
+
+## Running the preprocessor
+
+Simply create a folder with md files in this project, and add the following to book.toml
+
 ```
 
-Next you need to add the preprocessor to your `book.toml`
-```toml
-[book]
-authors = ["Your Name"]
-multilingual = false
-src = "src"
-title = "An Interesting Thing"
-[output.html]
+[preprocessor.vpreprocessor]
+command = "cargo run"
 
-[preprocessor.presentation-preprocessor]
 ```
 
-Now you can freely use the following syntax to mark
-sections of your book as web only or presentation only.
-```md
-# Header for the Page
-$web-only$
-This content should only be displayed when the page
-is in web presentation mode
-$web-only-end$
+Then you can run mdbook build, and the book will be outputted to the docs directory
+See /example for implementation.
 
-$slides-only$
-This content should only be displayed when the page
-is in slide presentation mode
-$slides-only-end$
-$notes$
-This content will always print to the console
-when this page is loaded. This can be useful
-when you break the dev-tools into its own window
-and keep that on presenter facing screen
-$notes-end$
-```
-To toggle between slides and web, you can press `alt+p`.
-
-A current limitation is that you would not be able to conditionally
-extend a bulleted or numbered list. Instead it will create a second
-list just below the first.
-
-Since MDBook doesn't offer paging from the `/` route, if you
-are in presentation mode and at `/`, it will automatically
-reload the page at the first entry in the navigation list.
-
-## How It Works
-
-The preprocessor does two things, first it replaces
-all of the directives with with HTML comments that
-have the same content.
-
-```md
-
-<!-- slides-only-->
-
-# Slides only information
-- is
-- found
-- here
-
-<!-- slides-only-end-->
-
-<!--web-only-->
-
-# Web only information
-Would be found here
-
-<!--web-only-end-->
-
-<!--notes
-Would be found here
--->
-```
-
-Since inline HTML is still valid markdown it shouldn't impact
-the rest of your book's ability to render.
-
-It also inserts some `css` as `js` to each page.
-
-The `js` does a few things, maintains a new `localStorage` variable
-`presentation_mode`. The value `1` is for web and the value `0`
-is for presentation. It also loops through the DOM, including the comments
-to add a new class to slides/web items and print the notes via `console.log`. This would make the above
-look like this
-
-```html
-<!-- slides-only-->
-<h1 class="presentation-only">Slides only information</h1>
-<ul class="presentation-only">
-    <li>is</li>
-    <li>found</li>
-    <li>here</li>
-</ul>
-<!-- slides-only-end-->
-<!--web-only-->
-<h1 class="article-content">Web only information</h1>
-<p class="article-content">Would be found here</p>
-<!--web-only-end-->
-<!--notes
-Would be found here
--->
-```
-
-It also updates these items to have another class that
-indicates if `presentation_mode` is `Web` or `Slides`.
-
-And adds an event listener for the `alt+p` shortcut.
-
-Depending on the value of `presentation_mode` it will update
-all of the wrapped items to have an additional class of
-either `presenting` or `not-presenting`. The `css`
-will set `display: hidden;` for any items that are
-`.article-content.presenting` or `.presentation-only.not-presenting`.
-
-Outside of that there is also a timer available. By default the timer is set to run 45 minutes, and will be mostly hidden on the right top side of the page and pop-out when moused over. The border will start black and slowly turn yellow, then orange then red as the end time approaches. To interact with this timer there are a few hot-keys to know.
-
-| hot-key       | action                           |
-|---------------|----------------------------------|
-| alt+g         | GO! (start the presentation)     |
-| alt+m         | Open the Minutes dialog          |
-| alt+ArrowUp   | Add a minute to the timer        |
-| alt+ArrowDown | Subtract a minute from the timer |
-| alt+.         | Full Stop the timer              |
-
-The end time and length are stored in localStorage, this means in the same browser you would expect the same length of presentation that was used previously.
-
-# Contributing
-If you are interested in contributing, I would be happy for the help
-
-If you are find a bug, please open an issue!
-
-If you want to make a change to the source code, please consider the following
-- The `./presentationHider.ts` file is where you should be editing any of the javascript
-    - To update the actual javascript, please run `tsc --outFile ./src/presentationHider.js`
-- The `./presentationHider.scss` file is where you should be editing any of the css
-    - To update the actual css please run your sass compiler of choice to update `./src/presentationHider.css`
-    - I personally use [`rsass`](https://crates.io/crates/rsass) with the following command
-        - `rsass ./presentationHider.scss > ./src/presentationHider.css`
-- When editing any of the rust functionality, please keep any of the logic outside of the `impl Preprocessor` block to allow the code to be tested
-
-For reference see [the api docs](https://github.com/FreeMasen/mdbook-presentation-preprocessor/blob/master/api_docs/api/presentationHider.md)
+Robert Masen's [presentation preprocessor](https://github.com/FreeMasen/mdbook-presentation-preprocessor) has been used to start this project.
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
-//! # MDBook Presentation Preprocessor
-//! A preprocessor for utilizing an MDBook as slides
-//! for a presentation.
+//! # MDBook Preprocessor for Vlang
+//! A preprocessor parsing MDBook compatible with Vlang
+//! to enable preproccessing in Vlang
 //!
-//! The goal of this project is to allow the writing of one
-//! MDBook that can be used as both presentation and me-book
-//! style website.
-//!
-//! ## Usage
-//! > book.toml
-//! ```toml
-//!
-//! ```
+//! The goal of this project is to allow preprocessing with Vlang
+//! by formatting deserialized mdbook, and sending encoded object via rmb.
+//! The processed book is then received via rmb and formatted back to 
+//! MDBook conventions ready for compilation.
+
 extern crate docopt;
 extern crate mdbook;
 extern crate serde_json;
@@ -19,41 +15,52 @@ extern crate serde_derive;
 extern crate serde;
 #[macro_use]
 extern crate log;
-extern crate chrono;
-extern crate env_logger;
+extern crate redis;
 
-use chrono::Local;
 use docopt::Docopt;
-use env_logger::Builder;
-use log::LevelFilter;
 use mdbook::{
     book::{Book, BookItem},
     errors::{Error, Result as MdResult},
     preprocess::{CmdPreprocessor, Preprocessor, PreprocessorContext},
 };
-use std::env;
+use redis::Commands;
 static USAGE: &str = "
 Usage:
-    mdbook-presentation-preprocessor
-    mdbook-presentation-preprocessor supports <supports>
+    mdbook-vpreprocessor
+    mdbook-vpreprocessor supports <supports>
 ";
+
+static NAME: &str = "mdbook-vpreprocessor";
 
 #[derive(Deserialize)]
 struct Args {
     pub arg_supports: Option<String>,
 }
 
-static NAME: &str = "mdbook-hide-presentation";
-static JS: &str = include_str!("./presentationHider.js");
-static CSS: &str = include_str!("./presentationHider.css");
+#[derive(serde::Serialize)]
+pub struct VBook {
+    pub sections: Vec<VBookItem>,
+    /* private fields */
+}
+
+#[derive(serde::Serialize)]
+pub struct VBookItem {
+    pub chapter: VChapter
+}
+
+#[derive(serde::Serialize)]
+pub struct VChapter {
+    pub name: String,
+    pub content: String,
+    pub sub_items: Vec<VBookItem>,
+}
 
 fn main() -> MdResult<()> {
-    init_logging();
     let args: Args = Docopt::new(USAGE)
         .and_then(|a| a.deserialize())
         .unwrap_or_else(|e| e.exit());
     info!("Running presentation preprocessor");
-    let pre = WrapPresentation;
+    let pre = VPreProcessor;
     if let Some(ref arg) = args.arg_supports {
         debug!("just getting support info {:?}", arg);
         if pre.supports_renderer(arg) {
@@ -69,16 +76,32 @@ fn main() -> MdResult<()> {
     Ok(())
 }
 
-struct WrapPresentation;
+struct VPreProcessor;
 
-impl Preprocessor for WrapPresentation {
+impl Preprocessor for VPreProcessor {
     fn name(&self) -> &str {
         NAME
     }
 
     fn run(&self, _ctx: &PreprocessorContext, mut book: Book) -> Result<Book, Error> {
-        debug!("Wrapping presentation only items with a div");
-        process_chapters(&mut book.sections)?;
+        debug!("Running preprocessor");
+        let res = send_book(&mut book);
+
+        /*
+        let vbook = format_book_to_vbook(book);
+        const mb = new MessageBusClient(6379)
+
+        const message = mb.prepare("mdbook.preprocess", 0)
+        mb.send(message, vbook)
+        mb.read(message, function (result) {
+            console.log("result received")
+            console.log(result)
+
+            console.log("closing")
+            process.exit(0)
+        })
+        */
+        
         Ok(book)
     }
 
@@ -90,157 +113,40 @@ impl Preprocessor for WrapPresentation {
     }
 }
 
-fn process_chapters<'a, I>(items: I) -> MdResult<()>
+// gets a vector of bookitems 
+// formats and returns a vection of vbookitems
+fn sections_to_vsections<'a, I>(items: I) -> Vec<VBookItem>
 where
     I: IntoIterator<Item = &'a mut BookItem> + 'a,
 {
+    //let mut sections: Vec<VBookItem>;
+    let mut vbookitems = Vec::new();
     for item in items {
         if let BookItem::Chapter(ref mut ch) = *item {
-            debug!("{}: processing chapter '{}'", NAME, ch.name);
-            let new_ch = replace(&ch.content, "web-only");
-            let new_ch = replace(&new_ch, "slides-only");
-            let new_ch = replace_with_parts(
-                &new_ch,
-                "$notes$",
-                "\n<!--notes",
-                "$notes-end$",
-                "-->",
-            );
-            let prefix = format!("<style>{}</style>\n\n", CSS);
-            let suffix = format!("\n\n<script>{}</script>", JS);
-            ch.content = format!("{}{}{}", prefix, new_ch, suffix);
-            process_chapters(&mut ch.sub_items)?;
+            let vbookitem = VBookItem {
+                chapter: VChapter {
+                    name: ch.name.clone(),
+                    content: ch.content.clone(),
+                    sub_items: sections_to_vsections(&mut ch.sub_items)
+                }
+            }; 
+            vbookitems.push(vbookitem);
         }
     }
+    return vbookitems;
+}
+
+fn format_book_to_vbook(book: &mut Book) -> VBook{
+    let vbook = VBook { sections: sections_to_vsections(&mut book.sections) };
+    return vbook;
+}
+
+fn send_book(book: &mut Book) -> redis::RedisResult<()> {
+    let client = redis::Client::open("redis://127.0.0.1:6379")?;
+    let formatted = format_book_to_vbook(book);
+    let mut con = client.get_connection()?;
+    let encoded = serde_json::to_string(&formatted).unwrap();
+    let _ : () = con.set("formatted_vbook", encoded)?;
+    //return(con)
     Ok(())
-}
-
-fn replace(s: &str, name: &str) -> String {
-    let start_tag = format!("${}$", name);
-    let start_comment = format!("\n<!--{}-->\n", name);
-    let end_tag = format!("${}-end$", name);
-    let end_comment = format!("\n<!--{}-end-->\n", name);
-    replace_with_parts(s, &start_tag, &start_comment, &end_tag, &end_comment)
-}
-
-fn replace_with_parts(s: &str, pat1: &str, rep1: &str, pat2: &str, rep2: &str) -> String {
-    let ret = s.replace(pat1, rep1);
-    ret.replace(pat2, rep2)
-}
-
-fn init_logging() {
-    use std::io::Write;
-    let mut builder = Builder::new();
-
-    builder.format(|formatter, record| {
-        writeln!(
-            formatter,
-            "{} [{}] ({}): {}",
-            Local::now().format("%Y-%m-%d %H:%M:%S"),
-            record.level(),
-            record.target(),
-            record.args()
-        )
-    });
-
-    if let Ok(var) = env::var("RUST_LOG") {
-        builder.parse_filters(&var);
-    } else {
-        // if no RUST_LOG provided, default to logging at the Info level
-        builder.filter(None, LevelFilter::Info);
-        // Filter extraneous html5ever not-implemented messages
-        builder.filter(Some("html5ever"), LevelFilter::Error);
-    }
-
-    builder.init();
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-    #[test]
-    fn basic_test() {
-        let md = "
-# Header
-- list
-- of
-- items
-
-$web-only$
-# web only header
-- web
-- only
-- list
-$web-only-end$
-$slides-only$
-# presenting only header
-- presenting
-- only
-- list
-$slides-only-end$
-";
-        let first_pass = replace(md, "web-only");
-
-        assert_eq!(
-            replace(&first_pass, "slides-only"),
-            r##"
-# Header
-- list
-- of
-- items
-
-
-<!--web-only-->
-
-# web only header
-- web
-- only
-- list
-
-<!--web-only-end-->
-
-
-<!--slides-only-->
-
-# presenting only header
-- presenting
-- only
-- list
-
-<!--slides-only-end-->
-
-"##
-        );
-    }
-
-    #[test]
-    fn empty_test() {
-        let test = r#"# Header
-- list
-- of
-- items
-"#;
-        assert_eq!(test, replace(test, "web-only"));
-    }
-
-    #[test]
-    fn notes_test() {
-        let test = r#"$notes$
-- notes
-- for the presentation
-- and stuff
-$notes-end$
-"#;
-        let expectation = r#"
-<!--notes
-- notes
-- for the presentation
-- and stuff
--->
-"#;
-        assert_eq!(expectation, replace_with_parts(test, "$notes$",
-                "\n<!--notes",
-                "$notes-end$",
-                "-->",));
-    }
 }
